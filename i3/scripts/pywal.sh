d741b0a4aae3d4f9a9488b8050b805d95cd1ca53
Filippo Barba <s223491@studenti.polito.it>
2018-09-04 13:47:12 +0200

    updated pywal script

--- i3/scripts/pywal.sh
+++ i3/scripts/pywal.sh
@@ -1,11 +1,21 @@
 #! /bin/bash
+if [[ $# -eq 1 ]]; then
+  if [[ $1 == "next" ]]; then
+    wal -i ~/Pictures > /dev/null &
+    ~/.local/bin/intellijpywal/intellijPywalGen.sh ~/.IntelliJIdea2018.2/config > /dev/null &
+    pic=$( cat $HOME/.cache/wal/wal )
+    betterlockscreen -u $pic > /dev/null &
+  fi
+  exit 0
+fi
 if pidof -o %PPID -x "pywal.sh">/dev/null; then
         exit -1
 fi
 while true
 do
-  wal -i ~/Pictures
-  sleep 0.3s
-  ~/.local/bin/intellijpywal/intellijPywalGen.sh ~/.IntelliJIdea2018.2/config
+  wal -i ~/Pictures > /dev/null &
+  ~/.local/bin/intellijpywal/intellijPywalGen.sh ~/.IntelliJIdea2018.2/config > /dev/null &
+  pic=$( cat $HOME/.cache/wal/wal )
+  betterlockscreen -u $pic > /dev/null
   sleep 10m
 done
